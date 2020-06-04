dd9e7d5e9b6acdbf64392ad46efc4f0213a67a78
Feyzullah Ertan <feyzullahertan@gmail.com>
2020-06-04 18:39:51 +0300

    made readable

--- getkNNValues.py
+++ getkNNValues.py
@@ -1,40 +1,39 @@
-import pandas as pd
 import xlrd
 import math
-
-location=r"C:\Users\FeyzullahErtan\Desktop\Knn\KNNData.xlsx"
-
-myList=[]
-wb = xlrd.open_workbook(location) 
-sheet = wb.sheet_by_index(0) 
-for i in range(sheet.nrows):
-    myList.append(sheet.row_values(i)) 
-#for x in myList[1:]:
-
-#print(myList[1][0])
-#print(myList[-1][0])
-main=myList[1]
+import pandas as pd
 
 
-u=[]
-for y in myList[1:]:
-    if main[6] == y[6]:
-        u.append(y)
-myResults=[]
-for allData in u[1:]:
-    firstColumn=(float(main[0])-float(allData[0]))**2
-    secondColumn=(float(main[1])-float(allData[1]))**2
-    thirdColumn=(float(main[2])-float(allData[2]))**2
-    forthColumn=(float(main[3])-float(allData[3]))**2
-    fifthColumn=(float(main[4])-float(allData[4]))**2
-    result=firstColumn+secondColumn+thirdColumn+forthColumn+fifthColumn
-    result=math.sqrt(result)
-    myResults.append(result)
-#print(myResults)
+location=r"KNNData.xlsx"
+data=[]
+def calculate():
+    wb = xlrd.open_workbook(location) 
+    sheet = wb.sheet_by_index(0) 
+    for i in range(sheet.nrows):
+        data.append(sheet.row_values(i)) 
+    main=data[1]
 
 
-minValue=min(myResults)
+    separatedData=[] 
+    for subset in data[1:]:
+        if main[6] == subset[6]:
+            separatedData.append(subset)
+    results=[]
 
-u=u[1:]
+    for allData in u[1:]:
+        firstColumn=(float(main[0])-float(allData[0]))**2
+        secondColumn=(float(main[1])-float(allData[1]))**2
+        thirdColumn=(float(main[2])-float(allData[2]))**2
+        forthColumn=(float(main[3])-float(allData[3]))**2
+        fifthColumn=(float(main[4])-float(allData[4]))**2
+        result=firstColumn+secondColumn+thirdColumn+forthColumn+fifthColumn
+        result=math.sqrt(result)
+        results.append(result)
 
-print("Sayi:  " + str(minValue) +"  -||- Index:  "+ str(myResults.index(minValue))+ "  Sinif Etiketi:  " + str(u[myResults.index(minValue)][7]))
+        minValue=min(results)
+
+        u=u[1:]
+    return str(minValue),str(results.index(minValue)),str(separatedData[results.index(minValue)][7])
+    # Number = minValue
+    # Index = results.index(minValue)
+    # Tag = separatedData[results.index(minValue)][7]
+    
